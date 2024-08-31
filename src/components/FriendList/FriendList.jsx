f8b3eaa611da9774b3be34dd6cce90b25cef2f60
Serhii Briushyn <serhii.briushyn@icloud.com>
2024-08-31 20:09:01 +0200

    react-hw-01

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import s from "./FriendList.module.css";
+import FriendListItem from "./FriendListItem";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={s.list}>
+      {friends.map((friend) => {
+        return (
+          <li className={s.item} key={friend.id}>
+            <FriendListItem
+              avatar={friend.avatar}
+              name={friend.name}
+              status={friend.isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default FriendList;
